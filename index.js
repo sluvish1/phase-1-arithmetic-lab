1a1f464975c344d804f4ac0fb0a185e68b2c870f
Vaschell Mkenzie <mvaschell@gmail.com>
2023-03-18 12:37:20 -0400

    Math Methods

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2 ;
 
-// Write your code here
+let random = 1
+Math.random();
+
+let num3 =  16
+let num4 = 6
+const mod = num3 % num4;
+
+let max = 20
+Math.max( 5, 10, 15, 20);
